17a7fb4dbd8215ecbd08e551f608cc37f9faa6b3
adhiyudanas <svarajati2_adhi@yahoo.co.id>
2023-09-03 11:01:36 +0900

    Update vue.config.js for GitHub Pages deployment

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+
+  // Adjust publicPath for GitHub Pages deployment
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/bard-kt-sample-pwa/'
+    : '/'
+});
